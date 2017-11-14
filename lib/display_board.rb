62baa2a53630b822feedd7cb24d65a2907394f19
John Burnett <tuninator@gmail.com>
2017-11-14 00:21:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  # Make nine cells, three to a row. C= "Cell", R = "Row". 
+  # Make nine cells, three to a row. C= "Cell", R = "Row".
   C1R1 = ["   |"]
   C2R1 = ["   |"]
   C3R1 = ["   "]
-  
+  C1R2 = ["   |"]
+  C2R2 = ["   |"]
+  C3R2 = ["   "]
+  C1R3 = ["   |"]
+  C2R3 = ["   |"]
+  C3R3 = ["   "]
